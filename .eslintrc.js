35ba6ea0814cb82483c9326ab03c9cb8229602a2
mpepes <piotr.michalek@stxnext.pl>
2020-04-22 21:14:49 +0200

    Basic react app configuration

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        'indent': ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-filename-extension': [1, {"extensions": [".js", ".jsx"]}],
+    },
+};
---
+++ app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => (
+    <div>
+        App
+        <img src="../smile.jpg" alt="" />
+    </div>
+);
+
+export default App;
