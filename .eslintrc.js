7baec91a770fb03c7ac27d3515da1c075ab0c22b
mpepes <piotr.michalek@stxnext.pl>
2020-04-23 22:09:48 +0200

    Configuration adjustments for webpack and linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     rules: {
         'indent': ['error', 4],
         'react/jsx-indent': ['error', 4],
-        'react/jsx-filename-extension': [1, {"extensions": [".js", ".jsx"]}],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-filename-extension': [1, {'extensions': ['.js', '.jsx']}],
     },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
-  resolve: {
-    modules: [__dirname, 'src', 'node_modules'],
-    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: require.resolve('babel-loader'),
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
+    entry: './index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
     ],
-  },
+    resolve: {
+        modules: [__dirname, 'app', 'node_modules'],
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: require.resolve('babel-loader'),
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
 };
